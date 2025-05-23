0603b3e81b676b64a5e8d9eaf719e1462f8b4352
Pramod <pramod4342@gmail.com>
2025-05-23 15:47:45 +0530

    mdl

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,6 +5,46 @@ const userSchema = new mongoose.Schema({
     user :{
         type : String,
         required : true,
+        unique : true,
+    },
+
+    email :{
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+
+    fullName :{
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+    },
+
+    avatar:{
+        type : String,
+        required : true,
+    },
+    coverImage : {
+        type : String,
+        
+    },  
+    password : {
+        type : String,
+        required : true,
+    },
+  
+    watchHistory :[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "video"
+        }
+    ],
+    
+    createdAt :{
+        type : Date,
+        default : Date.now,
     }
 })
 
