889fb97e0d3b71284593ff364cc62a75f2495314
Pramod <pramod4342@gmail.com>
2025-05-24 17:55:56 +0530

    .

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,28 +1,26 @@
-import {V2 as cloudinary} from "cloudinary";
+import { V2 as cloudinary } from "cloudinary";
 import { log } from "console";
 import fs from "fs";
 
- cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: CLOUDINARY_API_SECRET 
-        });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
 
+const cloudinaryUpload = async (localPath) => {
+  try {
+    if (!localPath) return null;
+    //upload the fle to cloudinary
+    const response = await cloudinary.uploader.upload(localPath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded to cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlink(localPath);
+    return null;
+  }
+};
 
-        const cloudinaryUpload = async (localPath)=>{
-            try {
-                if(!localPath) return null;
-                //upload the fle to cloudinary
-             const response = await cloudinary.uploader.upload(localPath,{
-                    resource_type : "auto"
-
-                });
-                console.log("file is uploaded to cloudinary",response.url);
-                return response;
-            } catch (error) {
-                fs.unlink(localPath);
-                return null;
-            }
-        }
-
-        export {cloudinaryUpload};
+export { cloudinaryUpload };
