ee6452ce97e69f2ec685e930e1fc19349c65d933
Pramod <pramod4342@gmail.com>
2025-05-28 20:58:51 +0530

    logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,28 @@
 import  asyncHandler  from "../utils/asyncHandler.js";
+import ApiError from "../utils/APierrHandle.js";
+import user from "../models/user.model.js";
 export const userRegister = asyncHandler(async (req,res)=>{
  
       const {fullName,email,password} = req.body;
        console.log("email:" , email);
        console.log("password",password)
-});
+
+       if ([fullName,email,password].some((fields)=>
+         fields?.trim() === "")
+       ) {
+            throw new ApiError(400, "All fields are required");
+       }
+      });
+      const ExistedUser = user.findOne({
+            $or :[{ email },{user} ]
+      })
+
+      if (ExistedUser) {
+            throw new ApiError(409, "User already exists");
+      }
+
+
+
 
 // controllers/user.controller.js
 // import asyncHandler from "../utils/asyncHandler.js";
@@ -13,4 +31,4 @@ export const userRegister = asyncHandler(async (req,res)=>{
 //   res.status(200).json({
 //     message: "ok",
 //   });
-// });
+// })
