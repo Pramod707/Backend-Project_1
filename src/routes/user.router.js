39223952a161db73c7c7ea17629e837aefe5d7b0
Pramod <pramod4342@gmail.com>
2025-05-29 22:59:17 +0530

    ....

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,8 +4,11 @@ import {upload} from "../middleWare/multer.middleware.js";
 const router = Router();
 router.route("/register").post(
      upload.fields([{
+
    name : "avataar",
+
    maxCount : 1,
+   
      },{
   name : coverImage,
 maxCount : 1,
